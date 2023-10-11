8e2f54ade56cccda37534a8660ba6c72e02e0e4d
Cody Cooper <cody_codster@live.com>
2023-10-11 00:16:28 -0500

    Created navbar and parallax scrolling for background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Navbar>
+      <div className='bgimg-1'></div>
+      <div className='bgimg-2'></div>
+      <div className='bgimg-3'></div>
+      <div className='bgimg-4'></div>
+      <Footer />
+      </Navbar>
     </div>
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,45 @@
 import React from "react";
+import logo from '../assets/nav-logo.png';
 
-export default function Navbar() {
+export default function Navbar({children}) {
   return ( 
-    <div className="navbar bg-base-100">
+<div className="drawer">
+  <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
+  <div className="drawer-content flex flex-col">
+    {/* Navbar */}
+    <div className="w-full navbar bg-black text-gray-200">
+      <div className="flex-none lg:hidden">
+        <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+        </label>
+      </div> 
+      <div className="flex-1 px-2 ml-8"><img src={logo}/></div>
+      <div className="flex-2 hidden md:block">
+        <ul className="menu menu-horizontal text-sm tracking-widest font-light space-x-5 mr-40">
+          {/* Navbar menu content here */}
+          <li><a>HOME</a></li>
+          <li><a>ABOUT</a></li>
+          <li><a>SERVICES</a></li>
+          <li><a>CREW</a></li>
+          <li><a>CONTACT</a></li>
+        </ul>
+      </div>
+    </div>
+    {children}
+  </div> 
+  <div className="drawer-side">
+    <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label> 
+    <ul className="menu p-4 w-80 min-h-full bg-black text-gray-200">
+      {/* Sidebar content here */}
+      <li><a>Sidebar Item 1</a></li>
+      <li><a>Sidebar Item 2</a></li>
+    </ul>
+  </div>
+</div>
+  ) };
+
+  /*
+  <div className="navbar bg-base-100">
   <div className="navbar-start">
     <div className="dropdown">
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -41,4 +78,5 @@ export default function Navbar() {
     <a className="btn">Button</a>
   </div>
 </div>
-  ) };
+  
+  */
