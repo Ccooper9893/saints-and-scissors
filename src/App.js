78e1a3b9f9181195444a868da0c497e9c8e63a36
Cody Cooper <cody_codster@live.com>
2023-10-12 23:19:58 -0500

    Updated styling and added new About section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import titleAndLogo from './assets/title-and-logo.png';
 
 function App() {
   return (
     <div>
       <Navbar>
-      <div className='bgimg-1'></div>
-      <div className='bgimg-2'></div>
-      <div className='bgimg-3'></div>
-      <div className='bgimg-4'></div>
-      <Footer />
+        <div className='bgimg-1'>
+          <div className='z-0 w-full'>
+            <img className='mx-auto mt-36' src={titleAndLogo} alt='Saints and Scissors Logo'></img>
+          </div>
+        </div>
+        <div className='bg-black h-96 text-center'>
+          <h1 className='title text-5xl md:text-7xl my-10 pb-4 mx-auto'>ABOUT S&S</h1>
+          <p className=' mx-10 lg:mx-60 text text-lg'>At Saints and Scissors, we are more than just hairstylists - we are artists, creators, and visionaries dedicated to transforming your hair into a work of art. With a passion for perfection and an unwavering commitment to making you look and feel your best, we have become a go-to destination for exceptional hair design and care.</p>
+        </div>
+        <div className='bgimg-2'></div>
+        <div className='bgimg-3'></div>
+        <div className='bgimg-4'></div>
+        <Footer />
       </Navbar>
     </div>
   );
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,32 +7,35 @@ export default function Navbar({children}) {
   <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
   <div className="drawer-content flex flex-col">
     {/* Navbar */}
-    <div className="w-full navbar bg-black text-gray-200">
+    <div className="w-full navbar bg-black text-gray-200 fixed z-50 lg:px-10 p-0">
       <div className="flex-none lg:hidden">
         <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
         </label>
       </div> 
-      <div className="flex-1 px-2 ml-8"><img src={logo}/></div>
-      <div className="flex-2 hidden md:block">
-        <ul className="menu menu-horizontal text-sm tracking-widest font-light space-x-5 mr-40">
+      <div className="flex-1 px-2 ml-8"><img className="navImg" src={logo}/></div>
+      <div className="flex-2 hidden lg:block">
+        <ul className="menu menu-horizontal text-sm tracking-widest font-light space-x-5 mr-40 p-0">
           {/* Navbar menu content here */}
-          <li><a>HOME</a></li>
-          <li><a>ABOUT</a></li>
-          <li><a>SERVICES</a></li>
-          <li><a>CREW</a></li>
-          <li><a>CONTACT</a></li>
+          <li><a className="hover py-0">HOME</a></li>
+          <li><a className="hover py-0">ABOUT</a></li>
+          <li><a className="hover py-0">SERVICES</a></li>
+          <li><a className="hover py-0">CREW</a></li>
+          <li><a className="hover py-0">CONTACT</a></li>
         </ul>
       </div>
     </div>
     {children}
   </div> 
-  <div className="drawer-side">
+  <div className="drawer-side z-50 w-48">
     <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label> 
-    <ul className="menu p-4 w-80 min-h-full bg-black text-gray-200">
+    <ul className="menu p-4 w-80 min-h-full bg-stone-900 text-gray-200">
       {/* Sidebar content here */}
-      <li><a>Sidebar Item 1</a></li>
-      <li><a>Sidebar Item 2</a></li>
+      <li><a className="hover">HOME</a></li>
+      <li><a className="hover">ABOUT</a></li>
+      <li><a className="hover">SERVICES</a></li>
+      <li><a className="hover">CREW</a></li>
+      <li><a className="hover">CONTACT</a></li>
     </ul>
   </div>
 </div>
