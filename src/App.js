d6b5b66fa6264e991c71a0368734e80cfa4ccb57
Cody Cooper <cody_codster@live.com>
2023-10-13 01:15:45 -0500

    Updated navbar

--- src/App.js
+++ src/App.js
@@ -6,21 +6,33 @@ import titleAndLogo from './assets/title-and-logo.png';
 function App() {
   return (
     <div>
-      <Navbar>
+      <Navbar />
         <div className='bgimg-1'>
           <div className='z-0 w-full'>
-            <img className='mx-auto mt-36' src={titleAndLogo} alt='Saints and Scissors Logo'></img>
+            <img className='mx-auto pt-36' src={titleAndLogo} alt='Saints and Scissors Logo'></img>
           </div>
         </div>
-        <div className='bg-black h-96 text-center'>
-          <h1 className='title text-5xl md:text-7xl my-10 pb-4 mx-auto'>ABOUT S&S</h1>
-          <p className=' mx-10 lg:mx-60 text text-lg'>At Saints and Scissors, we are more than just hairstylists - we are artists, creators, and visionaries dedicated to transforming your hair into a work of art. With a passion for perfection and an unwavering commitment to making you look and feel your best, we have become a go-to destination for exceptional hair design and care.</p>
+        <div className='bg-black text-center py-12'>
+          <h1 className='title text-5xl md:text-7xl mx-auto'>ABOUT S&S</h1>
+          <div className='hr'></div>
+          <p className=' mx-10 lg:mx-60 text text-lg pb-6'>At Saints and Scissors, we are more than just hairstylists - we are artists, creators, and visionaries dedicated to transforming your hair into a work of art. With a passion for perfection and an unwavering commitment to making you look and feel your best, we have become a go-to destination for exceptional hair design and care.</p>
         </div>
         <div className='bgimg-2'></div>
+        <div className='bg-black h-96 text-center'>
+        <button className="btn" onClick={()=>document.getElementById('my_modal_3').showModal()}>open modal</button>
+<dialog id="my_modal_3" className="modal">
+  <div className="modal-box">
+    <form method="dialog">
+      {/* if there is a button in form, it will close the modal */}
+      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+    </form>
+    <iframe className='tour' src="https://www.google.com/maps/embed?pb=!4v1697173625151!6m8!1m7!1sPp6lmciY0gob81SNNWVokw!2m2!1d33.49670251421148!2d-111.9313880580029!3f320!4f0!5f0.7820865974627469"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title='Saints and Scissors 360 Tour Google'></iframe>
+  </div>
+</dialog>
+        </div>
         <div className='bgimg-3'></div>
         <div className='bgimg-4'></div>
         <Footer />
-      </Navbar>
     </div>
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,41 +1,36 @@
 import React from "react";
 import logo from '../assets/nav-logo.png';
 
-export default function Navbar({children}) {
+export default function Navbar() {
   return ( 
-<div className="drawer">
-  <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
-  <div className="drawer-content flex flex-col">
-    {/* Navbar */}
-    <div className="w-full navbar bg-black text-gray-200 fixed z-50 lg:px-10 p-0">
-      <div className="flex-none lg:hidden">
-        <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-        </label>
-      </div> 
-      <div className="flex-1 px-2 ml-8"><img className="navImg" src={logo}/></div>
-      <div className="flex-2 hidden lg:block">
-        <ul className="menu menu-horizontal text-sm tracking-widest font-light space-x-5 mr-40 p-0">
-          {/* Navbar menu content here */}
-          <li><a className="hover py-0">HOME</a></li>
+<div className="navbar bg-black p-0 h-10 fixed">
+  <div className="lg:navbar-start">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content z-[1] bg-black w-52 text-gray-200 tracking-widestp-2">
+      <li><a className="hover py-2">HOME</a></li>
+          <li><a className="hover py-2">ABOUT</a></li>
+          <li><a className="hover py-2">SERVICES</a></li>
+          <li><a className="hover py-2">CREW</a></li>
+          <li><a className="hover py-2">CONTACT</a></li>
+      </ul>
+    </div>
+    <div className="lg:w-full">
+      <a>
+        <img className="w-auto lg:ml-48" src={logo} />
+      </a>
+    </div>
+
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1 p-0 text-gray-200 tracking-widest">
+    <li><a className="hover py-0">HOME</a></li>
           <li><a className="hover py-0">ABOUT</a></li>
           <li><a className="hover py-0">SERVICES</a></li>
           <li><a className="hover py-0">CREW</a></li>
           <li><a className="hover py-0">CONTACT</a></li>
-        </ul>
-      </div>
-    </div>
-    {children}
-  </div> 
-  <div className="drawer-side z-50 w-48">
-    <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label> 
-    <ul className="menu p-4 w-80 min-h-full bg-stone-900 text-gray-200">
-      {/* Sidebar content here */}
-      <li><a className="hover">HOME</a></li>
-      <li><a className="hover">ABOUT</a></li>
-      <li><a className="hover">SERVICES</a></li>
-      <li><a className="hover">CREW</a></li>
-      <li><a className="hover">CONTACT</a></li>
     </ul>
   </div>
 </div>
