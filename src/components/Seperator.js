eac373a3c3cbc85b70725e5ffd6ac3fa394f4010
Cody Cooper <cody_codster@live.com>
2023-10-19 16:36:07 -0500

    Fixed spacing issue between slides

--- src/components/Seperator.js
+++ src/components/Seperator.js
@@ -1,13 +1,12 @@
 export default function Seperator() {
     return (
-        <div className='relative w-screen h-screen -z-5'>
+        <div className='relative -z-5'>
         <div className="absolute bottom-0 w-full z-30">
             <div className="seperator w-full border-solid border-transparent border-t-20 border-l-1/2 border-r-1/2 bg-black">
             </div>
           </div>
           <div className='flex items-center justify-center'>
             <button className='absolute bottom-2 animate-bounce downIcon'>&#9660;</button>
-            <button className='absolute bottom-0 animate-bounce downIcon'>&#9660;</button>
           </div>
 
         </div>
