180f0ffeb0261dbca4f5c235cac4ad95761f42ec
Cody Cooper <cody_codster@live.com>
2023-10-20 04:27:22 -0500

    Polishing About page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import Navbar from './components/Navbar';
 import titleAndLogo from './assets/logos/title-and-logo.png';
 import Seperator from './components/Seperator';
-import aboutBg from './assets/backgrounds/slides-home-3.jpg';
-
+import aboutBg from './assets/backgrounds/slides-home-4.jpg';
+import Rating from './components/Rating';
 import amanda from './assets/stylists/amanda.png';
 import kendal from './assets/stylists/kendal.jpg';
 import cc from './assets/stylists/cc-1.jpg';
@@ -25,14 +25,30 @@ function App() {
         <Seperator />
 
         <div className="carousel-item backgroundAbout" id='about'>
-          <div className='h-full w-full bg-black pt-20 md:pt-20'>
-            <div className='flex flex-col justify-center w-4/5 lg:w-3/5 mt-6 mx-auto text-center'>
-              <h2 className='font2 text-4xl lg:text-5xl text-lime-600'>Unlock Your Hair's Potential.</h2>
+          <div className='h-full w-full bg-black pt-20 md:pt-14'>
+            <div className='flex flex-col justify-center lg:w-3/5 mt-6 mx-3 lg:mx-auto text-center'>
+              
+              <h2 className='font2 text-5xl text-white '>Unlock Your Hair's Potential.</h2>
               <hr className='w-4/5 lg:w-3/4 mx-auto mt-2 mb-4'></hr>
-              <p className='text-xl'>At Saints and Scissors, we are more than just hairstylists - we are artists, creators, and visionaries dedicated to transforming your hair into a work of art. With a passion for perfection and an unwavering commitment to making you look and feel your best, we have become a go-to destination for exceptional hair design and care.</p>
+              
+              <p className='text-lg'>At Saints and Scissors, we are more than just hairstylists - we are artists, creators, and visionaries dedicated to transforming your hair into a work of art. With a passion for perfection and an unwavering commitment to making you look and feel your best, we have become a go-to destination for exceptional hair design and care.</p>
+              <div className='flex flex-row align-middle justify-center my-2 text-center '>
+                <div>
+                  <Rating />
+                  <small><em>- Google</em></small>
+                </div>
+                <div>
+                  <Rating />
+                  <small><em>- Yelp</em></small>
+                </div>
+                <div>
+                  <Rating />
+                  <small><em>- Facebook</em></small>
+                </div>
+              </div>
             </div>
             <div className='relative w-full md:w-1/3 mt-12 lg:mt-6 mx-auto'>
-              <img src={aboutBg} alt='Inside of Saints and Scissors salon. Couch and multiple barber chairs. Coloristic interior design with an emphasize on a lime green, punk rock style.'></img>
+              <img class='border border-x-8 border-black' src={aboutBg} alt='Inside of Saints and Scissors salon. Couch and multiple barber chairs. Coloristic interior design with an emphasize on a lime green, punk rock style.'></img>
               <p className='absolute w-3/4 p-4 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-black bg-opacity-50'><em>“Life is more beautiful when you meet the right hairdresser.”<br></br><small className='mt-3 text-sm'> – Peter Coppola</small></em></p>
             </div>
           </div>
--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,12 +1,12 @@
 export default function Rating() {
     return (
         <div className="ratings">
-            <div className="rating">
-                <input type="radio" name="rating-1" className="mask mask-star" checked />
-                <input type="radio" name="rating-1" className="mask mask-star" checked />
-                <input type="radio" name="rating-1" className="mask mask-star" checked />
-                <input type="radio" name="rating-1" className="mask mask-star" checked />
-                <input type="radio" name="rating-1" className="mask mask-star" checked />
+            <div className="rating w-4/5">
+                <input type="radio" name="rating-1" className="mask mask-star bg-lime-500" checked />
+                <input type="radio" name="rating-1" className="mask mask-star bg-lime-500" checked />
+                <input type="radio" name="rating-1" className="mask mask-star bg-lime-500" checked />
+                <input type="radio" name="rating-1" className="mask mask-star bg-lime-500" checked />
+                <input type="radio" name="rating-1" className="mask mask-star bg-lime-500" checked />
             </div>
         </div>
     )
