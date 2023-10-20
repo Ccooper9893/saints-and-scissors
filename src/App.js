0e057fd7f24812319674a2638ecc141a9ecf1603
Cody Cooper <cody_codster@live.com>
2023-10-20 06:38:38 -0500

    Nuts and bolts

--- src/App.js
+++ src/App.js
@@ -27,14 +27,14 @@ function App() {
         <div className="carousel-item backgroundAbout" id='about'>
           <div className='h-full w-full bg-black pt-28 md:pt-14'>
             <div className='flex flex-col justify-center lg:w-3/5 mt-6 mx-3 lg:mx-auto text-center'>
-              
+
               <h2 className='font2 text-5xl text-white mb-4'>Unlock Your Hair's Potential.</h2>
               <p className='text-lg'>At Saints and Scissors, we are more than just hairstylists - we are <strong>artists</strong>, <strong>creators</strong>, and <strong>visionaries</strong> dedicated to transforming your hair into a work of art. With a passion for perfection and an unwavering commitment to making you look and feel your best, we have become a go-to destination for exceptional hair design and care.</p>
               <hr className='w-4/5 lg:w-3/4 mx-auto my-2'></hr>
               <div className='flex flex-col justify-center my-4 text-center '>
                 <p class='backdrop-brightness-50 font2 text-lg'>"Best salon experience I have ever had."</p>
-                  <div className='mx-auto'><Rating /></div>
-                  <small><em>- Ashely A.</em></small>
+                <div className='mx-auto'><Rating /></div>
+                <small><em>- Ashely A.</em></small>
               </div>
 
             </div>
@@ -50,34 +50,34 @@ function App() {
         <div className="carousel-item bg-black" id='stylists'>
           <div className='h-full w-full pt-28 md:pt-20'>
             <div className='flex flex-col justify-center w-full lg:w-3/5 mt-6 mx-auto text-center'>
-              <h2 className='font2 text-5xl text-white mb-4'>Discover Our Stylist Team</h2>
+              <h2 className='font2 text-3xl text-white mb-4'>From Classic to Creative: Our Stylists Have You Covered</h2>
               <hr className='w-3/4 mx-auto mb-4'></hr>
               <div className='flex flex-row flex-wrap lg:mx-28 justify-center text-4xl'>
-          <div className='p-3 w-52'>
-            <img className='selfie' src={sherri} alt='Sherri headshot'></img>
-            <h2 className='title'>Sherri</h2>
-          </div>
-          <div className='p-3 w-52'>
-            <img className='selfie' src={paul} alt='Sherri headshot'></img>
-            <h2 className='title'>Paul</h2>
-          </div>
-          <div className='p-3 w-52'>
-            <img className='selfie' src={cc} alt='Sherri headshot'></img>
-            <h2 className='title'>CC</h2>
-          </div>
-          <div className='p-3 w-52'>
-            <img className='selfie' src={kendal} alt='Sherri headshot'></img>
-            <h2 className='title'>Kendal</h2>
-          </div>
-          <div className='p-3 w-52'>
-            <img className='selfie' src={juli} alt='Sherri headshot'></img>
-            <h2 className='title'>Juli</h2>
-          </div>
-          <div className='p-3 w-52'>
-            <img className='selfie' src={amanda} alt='Sherri headshot'></img>
-            <h2 className='title'>Amanda</h2>
-          </div>
-        </div>
+                <div className='p-3 w-52'>
+                  <img className='selfie' src={sherri} alt='Sherri headshot'></img>
+                  <h2 className='title'>Sherri</h2>
+                </div>
+                <div className='p-3 w-52'>
+                  <img className='selfie' src={paul} alt='Sherri headshot'></img>
+                  <h2 className='title'>Paul</h2>
+                </div>
+                <div className='p-3 w-52'>
+                  <img className='selfie' src={cc} alt='Sherri headshot'></img>
+                  <h2 className='title'>CC</h2>
+                </div>
+                <div className='p-3 w-52'>
+                  <img className='selfie' src={kendal} alt='Sherri headshot'></img>
+                  <h2 className='title'>Kendal</h2>
+                </div>
+                <div className='p-3 w-52'>
+                  <img className='selfie' src={juli} alt='Sherri headshot'></img>
+                  <h2 className='title'>Juli</h2>
+                </div>
+                <div className='p-3 w-52'>
+                  <img className='selfie' src={amanda} alt='Sherri headshot'></img>
+                  <h2 className='title'>Amanda</h2>
+                </div>
+              </div>
             </div>
           </div>
         </div>
