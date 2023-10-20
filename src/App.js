eaa8200e722d029ee872e448afc9c66f98feb724
Cody Cooper <cody_codster@live.com>
2023-10-20 11:27:56 -0500

    Making progress on About section layout

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import titleAndLogo from './assets/logos/title-and-logo.png';
 import Seperator from './components/Seperator';
 import aboutBg from './assets/backgrounds/paintedGirl.png';
 import Rating from './components/Rating';
+import Prices from './components/Prices';
 import amanda from './assets/stylists/amanda.png';
 import kendal from './assets/stylists/kendal.jpg';
 import cc from './assets/stylists/cc-1.jpg';
@@ -46,6 +47,23 @@ function App() {
         </div>
 
         <Seperator />
+
+        <div className="carousel-item" id='about'>
+          <div className='h-full w-full bg-black pt-28 md:pt-14'>
+            <div className='flex flex-col justify-center lg:w-3/5 mt-6 mx-3 lg:mx-auto text-center'>
+
+              <h2 className='font2 text-5xl text-white mb-4'>Services</h2>
+              <Prices />
+
+            </div>
+            <div className='relative w-full md:w-1/4 mt-5 lg:mt-6 mx-auto'>
+              <img class='opacity-80' src={aboutBg} alt='Grunge style women'></img>
+              {/* <p className='absolute w-3/4 p-4 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-black bg-opacity-75'><em>“Life is more beautiful when you meet the right hairdresser.”<br></br><small className='mt-3 text-sm'> – Peter Coppola</small></em></p> */}
+            </div>
+          </div>
+        </div>
+
+<Seperator />
 
         <div className="carousel-item background3" id='stylists'>
           <div className='h-full w-full pt-28 md:pt-20'>
