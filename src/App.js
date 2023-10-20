74c568e231016ab5c95265fa8466062d2230e245
Cody Cooper <cody_codster@live.com>
2023-10-20 05:24:05 -0500

    Replaced the ugly generic square picture with something better

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import titleAndLogo from './assets/logos/title-and-logo.png';
 import Seperator from './components/Seperator';
-import aboutBg from './assets/backgrounds/slides-home-4.jpg';
+import aboutBg from './assets/backgrounds/paintedGirl.png';
 import Rating from './components/Rating';
 import amanda from './assets/stylists/amanda.png';
 import kendal from './assets/stylists/kendal.jpg';
@@ -28,28 +28,19 @@ function App() {
           <div className='h-full w-full bg-black pt-28 md:pt-14'>
             <div className='flex flex-col justify-center lg:w-3/5 mt-6 mx-3 lg:mx-auto text-center'>
               
-              <h2 className='font2 text-5xl text-white '>Unlock Your Hair's Potential.</h2>
-              <hr className='w-4/5 lg:w-3/4 mx-auto mt-2 mb-4'></hr>
-              
-              <p className='text-lg'>At Saints and Scissors, we are more than just hairstylists - we are artists, creators, and visionaries dedicated to transforming your hair into a work of art. With a passion for perfection and an unwavering commitment to making you look and feel your best, we have become a go-to destination for exceptional hair design and care.</p>
-              <div className='flex flex-row align-middle justify-center my-2 text-center '>
-                <div>
-                  <Rating />
-                  <small><em>- Google</em></small>
-                </div>
-                <div>
-                  <Rating />
-                  <small><em>- Yelp</em></small>
-                </div>
-                <div>
-                  <Rating />
-                  <small><em>- Facebook</em></small>
-                </div>
+              <h2 className='font2 text-5xl text-white mb-4'>Unlock Your Hair's Potential.</h2>
+              <p className='text-lg'>At Saints and Scissors, we are more than just hairstylists - we are <strong>artists</strong>, <strong>creators</strong>, and <strong>visionaries</strong> dedicated to transforming your hair into a work of art. With a passion for perfection and an unwavering commitment to making you look and feel your best, we have become a go-to destination for exceptional hair design and care.</p>
+              <hr className='w-4/5 lg:w-3/4 mx-auto my-2'></hr>
+              <div className='flex flex-col justify-center my-4 text-center '>
+                <p class='backdrop-brightness-50 font2 text-lg'>"Best salon experience I have ever had."</p>
+                  <div className='mx-auto'><Rating /></div>
+                  <small><em>- Ashely A.</em></small>
               </div>
+
             </div>
-            <div className='relative w-full md:w-1/3 mt-12 lg:mt-6 mx-auto'>
-              <img class='border border-x-8 border-black' src={aboutBg} alt='Inside of Saints and Scissors salon. Couch and multiple barber chairs. Coloristic interior design with an emphasize on a lime green, punk rock style.'></img>
-              <p className='absolute w-3/4 p-4 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-black bg-opacity-50'><em>“Life is more beautiful when you meet the right hairdresser.”<br></br><small className='mt-3 text-sm'> – Peter Coppola</small></em></p>
+            <div className='relative w-full md:w-1/3 mt-5 lg:mt-6 mx-auto'>
+              <img class=' opacity-80' src={aboutBg} alt='Inside of Saints and Scissors salon. Couch and multiple barber chairs. Coloristic interior design with an emphasize on a lime green, punk rock style.'></img>
+              {/* <p className='absolute w-3/4 p-4 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-black bg-opacity-75'><em>“Life is more beautiful when you meet the right hairdresser.”<br></br><small className='mt-3 text-sm'> – Peter Coppola</small></em></p> */}
             </div>
           </div>
         </div>
