34c9ea4869005fe20e757da17a79bcb01d39e387
Cody Cooper <cody_codster@live.com>
2023-10-23 00:08:14 -0500

    Button bs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -72,7 +72,7 @@ export default function Navbar({ children, currentPage, handlePageChange, availa
 
         {/* BOOK NOW BANNER BUTTON */}
           <div className="absolute w-36 md:w-44 bottom-44 z-10 left-1/2 transform -translate-x-1/2">
-            <button className="normal-case w-full py-1 text-lg bg-black text-lime-400" onClick={() => { toggleBooking(true); }} aria-label="Open Vagaro booking drawer">BOOK NOW</button>
+            <button className="normal-case w-full py-1 text-lg shadow-sm text-white bg-black shadow-lime-400" onClick={() => { toggleBooking(true); }} aria-label="Open Vagaro booking drawer">BOOK NOW</button>
           </div>
 
 
