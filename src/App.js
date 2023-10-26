442932c31b1db3cade402e3b59f487c8a2b28e86
Cody Cooper <cody_codster@live.com>
2023-10-26 10:39:08 -0500

    Testing

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ export default function App() {
       <Navbar toggleBooking={toggleBooking} drawerRef2={drawerRef2} />
 
 
-        <img className='fixed mx-auto z-10 mt-44 centerOfBg' src={titleAndLogo} alt='Saints and Scissors Logo'></img>
+        <img className='fixed mx-auto z-10 mt-80 centerOfBg md:1/2 lg:w-1/3' src={titleAndLogo} alt='Saints and Scissors Logo'></img>
 
       {/* </Parallax> */}
       <Parallax speed={-5}>
