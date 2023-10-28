e66bbb5449c7cced5b9e6731dda3dbddd0f0fdfb
Cody Cooper <cody_codster@live.com>
2023-10-28 10:12:04 -0500

    Installed framer motion

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,7 @@ export default function Navbar({ drawerRef2, toggleBooking }) {
             <div className="drawer-content flex flex-col">
 
                 {/* Navbar */}
-                <div className="fixed w-full navbar justify-between md:justify-around py-0 bg-stone-900 bg-opacity-90">
+                <div className="fixed w-full navbar hidden justify-between md:justify-around py-0 bg-stone-900 bg-opacity-90">
                     <div className="flex justify-start lg:hidden w-24">
                         <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,20 +1,24 @@
 import titleAndLogo from '../../assets/img/logos/title-and-logo.png';
 import titleAndLogoV from '../../assets/img/logos/title-vertical-2.png';
 import Carousel from '../misc/Carousel';
-export default function Home({toggleBooking}) {
+import { Parallax } from 'react-scroll-parallax';
+
+export default function Home({ toggleBooking }) {
     return (
         <>
             <div className='hidden lg:block'>
-                <img className='absolute centerOfBg mx-auto z-10 mt-80 md:1/2 lg:w-1/2' src={titleAndLogo} alt='Saints and Scissors Logo'></img>
-                <button onClick={() => { toggleBooking(true); }} className='absolute centerOfBg top-3/4 tracking-widest z-10 w-60 py-1 text-black text-center text-sm bg-lime-400 bg-opacity-90 shadow shadow-black hover:shadow-none'>BOOK NOW</button>
+                <img className='absolute centerOfBg mx-auto z-10 mt-80 md:1/2 lg:w-1/3' src={titleAndLogo} alt='Saints and Scissors Logo'></img>
+                <button onClick={() => { toggleBooking(true); }} className='absolute centerOfBg top-3/4 tracking-widest z-10 w-60 py-1 rounded-sm text-black text-center text-sm bg-lime-400 bg-opacity-90 shadow shadow-black hover:bg-lime-400'>BOOK NOW</button>
             </div>
 
-
-            <img className='fixed lg:hidden centerOfBg z-10 mt-title max-h-5/6' src={titleAndLogoV} alt='Saints and Scissors Logo'></img>
-            <button onClick={() => { toggleBooking(true); }} className='absolute lg:hidden right-0 bottom-10 tracking-widest z-10 w-64 py-2 text-black text-center text-sm bg-lime-400 bg-opacity-70 shadow shadow-black hover:shadow-none'>BOOK NOW</button>
-
-            <Carousel />
-
+            <div>
+                <img className='fixed lg:hidden centerOfBg z-10 mt-title max-h-5/6' src={titleAndLogoV} alt='Saints and Scissors Logo'></img>
+                <p className='absolute lg:hidden right-0 bottom-16 z-10 w-72 text-right py-2 text-md text-white'>A NEW BREED OF SALON<br /><span className='text-xs'>WHERE THE HAIR IS AS IMPORTANT AS THE ATMOSTPHERE.</span></p>
+                <button onClick={() => { toggleBooking(true); }} className='absolute lg:hidden right-0 bottom-4 tracking-widest z-10 w-64 py-2 rounded-sm text-black text-center text-sm bg-lime-400 bg-opacity-70 shadow-lg shadow-black hover:shadow-none'>BOOK NOW</button>
+            </div>
+            <Parallax speed={-10}>
+                <Carousel />
+            </Parallax>
         </>
     )
 };
