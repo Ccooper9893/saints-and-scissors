a1e3f0b7abf901177a56d66e1be62aaf009b2658
Cody Cooper <cody_codster@live.com>
2023-10-31 10:11:05 -0500

    Messing around with homepage backgrounds

--- src/components/misc/Carousel.js
+++ src/components/misc/Carousel.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import slide1 from '../../assets/img/backgrounds/slides-home-1.jpg';
-import slide2 from '../../assets/img/backgrounds/slides-home-2.jpg';
-import slide3 from '../../assets/img/backgrounds/slides-home-3.jpg';
+import slide1 from '../../assets/img/backgrounds/slides-home-1-dark-bottom.jpg';
+import slide2 from '../../assets/img/backgrounds/slides-home-2-dark-bottom.jpg';
+import slide3 from '../../assets/img/backgrounds/slides-home-3-dark-bottom.jpg';
 
 export default function Carousel() {
   const settings = {
