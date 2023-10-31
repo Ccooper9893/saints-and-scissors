9e77e3e6158f4558b70c307ebcdaa37f85bd0334
Cody Cooper <cody_codster@live.com>
2023-10-31 15:01:54 -0500

    Messing with backgrounds

--- src/components/misc/Carousel.js
+++ src/components/misc/Carousel.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import slide1 from '../../assets/img/backgrounds/slides-home-1-dark-bottom.jpg';
+import slide1 from '../../assets/img/backgrounds/slides-home-1-white-bottom.jpg';
 import slide2 from '../../assets/img/backgrounds/slides-home-2-dark-bottom.jpg';
 import slide3 from '../../assets/img/backgrounds/slides-home-3-dark-bottom.jpg';
 
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,22 +5,22 @@ import logo from '../../assets/img/logos/logo.png';
 
 export default function About({ navAboutRef }) {
   return (
-    <div id='about' className='h-full bg-stone-950 text-stone-300'>
+    <div id='about' className='h-full bg-stone-200 text-stone-950'>
       <div className='pt-4 py-12'>
         <div className='flex flex-col justify-center lg:w-3/4 mt-6 lg:mx-auto text-center'>
           <h2 className='font2 text-6xl lg:text-9xl center text-stone-200 pb-4 md:mb-24 z-10 bg-stone-950'>
             Unlock
             Your Hair's< br />
-            <span className='text-violet-500 font2'>Potential</span>.
+            <span className='text-violet-700 font2'>Potential</span>.
           </h2>
           <div className='flex flex-col'>
-            <div className='relative px-8 w-full bg2'>
+            <div className='relative px-8 w-full'>
               <Parallax speed={-4}>
-                <img className='mx-auto z-0 bg-stone-300 bg-opacity-80 rotate-2' src={aboutBg} alt='Grunge style women'></img>
+                <img className='mx-auto z-0 bg-stone-950 bg-opacity-80 rotate-2' src={aboutBg} alt='Grunge style women'></img>
               </Parallax>
               {/* <p className='absolute w-3/4 p-4 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-black bg-opacity-75'><em>“Life is more beautiful when you meet the right hairdresser.”<br></br><small className='mt-3 text-sm'> – Peter Coppola</small></em></p> */}
             </div>
-            <div className='py-6 md:py-24 bg-stone-900 border border-stone-700 shadow shadow-bg-stone-900 z-10'>
+            <div className='py-6 md:py-24 bg-stone-200 z-10'>
               <p className='text-xl md:text-4xl md:w-5/6 md:text-center md:mx-auto p-4 tracking-wide'>At <span className='font4'>Saints and Scissors,</span><br />we are more than just hairstylists - we are <strong>artists</strong>, <strong>creators</strong>, and <strong>visionaries</strong> dedicated to transforming your hair into a work of art. With a passion for perfection and an unwavering commitment to making you look and feel your best, we have become a go-to destination for exceptional hair design and care.</p>
 
               <div className='flex flex-row justify-center rounded-r-lg my-12'>
