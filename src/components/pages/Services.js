07285e67af8d09dcc07826ba67f82eeb4a761d76
Cody Cooper <cody_codster@live.com>
2023-11-05 02:10:38 -0600

    bgs

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -4,7 +4,7 @@ import girl from '../../assets/img/backgrounds/paintedGirl.png';
 import { Parallax } from 'react-scroll-parallax';
 export default function Services({ toggleBooking, navServicesRef }) {
     return (
-        <div id='services' className=' bg-stone-100 shadow shadow-stone-900'>
+        <div id='services' className=' bg-stone-200 shadow shadow-stone-900 border-b border-stone-900'>
             <div ref={navServicesRef}>
                 <div className='flex flex-col lg:flex-row mt-12 md:py-24'>
                     <div className='hidden h-1/2 md:block md:ml-4 md:p-8 '>
@@ -14,15 +14,15 @@ export default function Services({ toggleBooking, navServicesRef }) {
                         <img className='bg-stone-200 pt-12 opacity-80' src={girl} alt='Grunge style headshot of a woman with beautiful flowing hair.'></img>
                     </div>
                     <div className='mt-6 md:mt-28 md:mx-2 md:w-3/5 text-center'>
-                        <h2 className='text-stone-900 font2 py-4 text-5xl md:text-5xl mx-4 md:px-16 uppercase'>Tailored Services for Your <span className='text-violet-600 font2'>Unique</span> Style</h2>
-                        
-                        <p className='text-xl md:right md:mx-auto p-2 tracking-wide text-stone-700'>Whether you're seeking a fresh, on-trend haircut, a bold and vibrant color transformation, or a classic, timeless look, our team is dedicated to creating a personalized, unparalleled experience that leaves you feeling confident, beautiful, and absolutely unique. We offer a wide range of services to cater to your unique preferences and needs.</p>
+                        <h2 className='text-stone-900 font2 py-4 text-5xl md:text-5xl mx-4 md:px-16 uppercase'>Tailored Services for Your <span className='text-violet-500 font2'>Unique</span> Style</h2>
+
+                        <p className='text-xl md:right md:mx-auto py-2 px-10 tracking-wide text-stone-700'>Whether you're seeking a fresh, on-trend haircut, a bold and vibrant color transformation, or a classic, timeless look, our team is dedicated to creating a personalized, unparalleled experience that leaves you feeling confident, beautiful, and absolutely unique. We offer a wide range of services to cater to your unique preferences and needs.</p>
                         <div className='flex justify-center my-20'>
-                            <a
-                                href='#contact'
+                            <button
+                                onClick={() => { toggleBooking(true); }}
                                 className=' px-8 py-2 border border-stone-900 text-stone-900 hover:bg-stone-300'>
-                                CONTACT US
-                            </a>
+                                VIEW OUR SERVICES
+                            </button>
                         </div>
                         {/* <hr className='mx-6 mt-16 bg-stone-950'></hr>
                         <div className='w-5/6 md:w-full py-12 md:py-4 md:bg-stone-300 mx-auto z-0'>
